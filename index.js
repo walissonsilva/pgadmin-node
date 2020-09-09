e5d7eb82d22c89260899e60d5f5fdf97d72af2a5
Walisson Silva <walissonsilva10@gmail.com>
2020-09-09 11:05:48 -0300

    Front-end done

--- index.js
+++ index.js
@@ -1,14 +1,23 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 const ejs = require('ejs');
-const { Client } = require('pg');
 
 const app = express();
 
 app.set('view engine', 'ejs');
 app.use('/', express.static('public'));
+app.use(bodyParser.urlencoded({ extended: false }))
 
 app.get('/', (req, res) => {
     res.render('index', {query: false});
+})
+
+app.post('/query', (req, res) => {
+  let query = req.body.query;
+  
+  res.render('index', {query: true, results: query})
+
+  console.log(query);
 })
 
 app.listen(3000, () => {
